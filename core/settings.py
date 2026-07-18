71cafce4ca5a8484eb4e774704684b3362e44afa
Cesar Hernandez <cesar.hernandezaparicio@cesunbc.edu.mx>
2026-07-18 09:42:13 -0700

    statics para las imagenes

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Archivos multimedia (fotos de los reportes)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import RecursoViewSet
 from api.views import ReporteFugaViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'resources', RecursoViewSet, basename='recurso')
@@ -12,3 +14,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    
